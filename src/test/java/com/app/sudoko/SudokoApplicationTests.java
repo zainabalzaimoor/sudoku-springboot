0e1bab0af9e36250f5707f3d58e891ef8e13c8c1
zainabalzaimoor <alzaimoorzainab@gmail.com>
2026-02-04 21:42:25 +0300

    Initial commit

---
+++ src/test/java/com/app/sudoko/SudokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sudoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
